3b0801b103dd32ad6337032e4258607c007f1ea3
David Mendez Guardado <demg@outlook.com>
2018-09-26 11:43:49 -0500

    -Update the setup.py

--- setup.py
+++ setup.py
@@ -17,8 +17,4 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'hashlib',
-        'datetime',
-    ],
 )
